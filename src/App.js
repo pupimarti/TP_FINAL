f25e92675cfbfd280e84709f89d2a6a03f812ad9
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-08-17 16:12:55 -0300

    add connection firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getPlaces } from './firebaseController';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          TP Final
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  getPlaces();
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>TP Final</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/firebaseController.js
@@ -0,0 +1,32 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const config = {
+	apiKey: 'AIzaSyDhlOiJG6Zx1bcqUP2EkKJ2MLFv1vLgd4k',
+	authDomain: 'pinamar-pide.firebaseapp.com',
+	databaseURL: 'https://pinamar-pide.firebaseio.com',
+	projectId: 'pinamar-pide',
+	storageBucket: 'pinamar-pide.appspot.com',
+	messagingSenderId: '268459213561',
+	appId: '1:268459213561:web:80b1dc8d0b7e8032cb9828',
+};
+
+const app = firebase.initializeApp(config);
+
+export const getPlaces = () => {
+    
+	return app
+		.firestore()
+		.collection('places')
+		.get()
+		.then((db_get) => {
+			let db_data = [];
+            db_get.forEach((doc) => db_data.push({ ...doc.data(), id: doc.id }));
+            console.log(db_data);
+			return db_data;
+		})
+		.catch((e) => {
+			console.log(e);
+			throw new Error('Ocurrió un error. Vuelva a intentarlo más tarde.');
+		});
+};
