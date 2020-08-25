73b03e4e599fce3b4bd648aa2732a8ed12ab6145
Juan Agustín Marti <pupi_marti@hotmail.com>
2020-08-25 11:52:17 -0300

    changes item list

--- src/components/List/Item/index.js
+++ src/components/List/Item/index.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
 
-export default function Item() {
-    return (
-        <div>
-            item
+import { ReactComponent as Phone } from "img/telefono.svg";
+
+import { Img } from "react-image";
+import Loading from "components/Loading";
+import { Link } from "react-router-dom";
+
+function Item(props) {
+  return (
+    <div className="container-list-item">
+      <Link to={"/" + props.id} className="list-item ripple">
+        <div className="list-container-img">
+          <Img
+            src={[
+              "https://firebasestorage.googleapis.com/v0/b/pinamar-pide.appspot.com/o/logos%2F" +
+                props.id +
+                ".jpg?alt=media",
+              "https://firebasestorage.googleapis.com/v0/b/pinamar-pide.appspot.com/o/telefono.svg?alt=media",
+            ]}
+            className="list-img"
+            loader={<Loading small />}
+          />
         </div>
-    )
+
+        <div className="list-container-name-info">
+          <h3 className="list-name">{props.name}</h3>
+          <p className="list-info">{props.phone}</p>
+        </div>
+      </Link>
+      {props.phone && (
+        <a
+          href={"tel:+549" + props.phone}
+          className="container-button-list-item"
+        >
+          <Phone className="list-phone" />
+        </a>
+      )}
+    </div>
+  );
 }
+
+export default React.memo(Item);
--- src/firebaseController.js
+++ src/firebaseController.js
@@ -1,32 +1,30 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 const config = {
-	apiKey: 'AIzaSyDhlOiJG6Zx1bcqUP2EkKJ2MLFv1vLgd4k',
-	authDomain: 'pinamar-pide.firebaseapp.com',
-	databaseURL: 'https://pinamar-pide.firebaseio.com',
-	projectId: 'pinamar-pide',
-	storageBucket: 'pinamar-pide.appspot.com',
-	messagingSenderId: '268459213561',
-	appId: '1:268459213561:web:80b1dc8d0b7e8032cb9828',
+  apiKey: "AIzaSyDhlOiJG6Zx1bcqUP2EkKJ2MLFv1vLgd4k",
+  authDomain: "pinamar-pide.firebaseapp.com",
+  databaseURL: "https://pinamar-pide.firebaseio.com",
+  projectId: "pinamar-pide",
+  storageBucket: "pinamar-pide.appspot.com",
+  messagingSenderId: "268459213561",
+  appId: "1:268459213561:web:80b1dc8d0b7e8032cb9828",
 };
 
 const app = firebase.initializeApp(config);
 
-export const getPlaces = () => {
-    
+export const getPlaces = (type) => {
 	return app
 		.firestore()
-		.collection('places')
+		.collection(type)
 		.get()
 		.then((db_get) => {
 			let db_data = [];
-            db_get.forEach((doc) => db_data.push({ ...doc.data(), id: doc.id }));
-            console.log(db_data);
+			db_get.forEach((doc) => db_data.push({ ...doc.data(), id: doc.id }));
 			return db_data;
 		})
-		// .catch((e) => {
-		// 	console.log(e);
-		// 	throw new Error('Ocurrió un error. Vuelva a intentarlo más tarde.');
-		// });
+		.catch((e) => {
+			console.log(e);
+			throw new Error('Ocurrió un error. Vuelva a intentarlo más tarde.');
+		});
 };
