b9d656645ec1427f400163906317ccc3787138f6
Octavio Amietta <octavioa1997@gmail.com>
2020-09-15 16:45:19 -0300

    edit 1.0

--- src/components/Account/Edit/index.js
+++ src/components/Account/Edit/index.js
@@ -1,21 +1,11 @@
 import React, { useState } from "react";
 import "./css.css";
 
-import { useLocation, Link } from "react-router-dom";
-import Loading from "components/Loading";
+import { Link } from "react-router-dom";
 
 import logo from "img/logo-completo.png";
-import phone from "img/telefono.svg";
-import { ReactComponent as Whatsapp } from "img/whatsapp.svg";
-import { ReactComponent as Facebook } from "img/facebook.svg";
-import { ReactComponent as Instagram } from "img/instagram.svg";
-import { ReactComponent as Share } from "img/share.svg";
-import { ReactComponent as Location } from "img/location.svg";
-import { Img } from "react-image";
 
 export default function Edit() {
-  const id = useLocation().pathname.substr(1);
-
   const [data] = useState("loading");
 
   return (
@@ -28,166 +18,49 @@ export default function Edit() {
           <img src={logo} alt="logo" className="navbar-logo" />
         </Link>
       </header>
-      {data === null ? (
-        <h1>Sitio incorrecto</h1>
-      ) : data === "loading" ? (
-        <Loading />
-      ) : (
-        <div className="profile-container-place fade-in">
-          <Img
-            src={[
-              "https://firebasestorage.googleapis.com/v0/b/pinamar-pide.appspot.com/o/logos%2F" +
-                id +
-                ".jpg?alt=media",
-              "https://firebasestorage.googleapis.com/v0/b/pinamar-pide.appspot.com/o/telefono.svg?alt=media",
-            ]}
-            alt={data.name}
-            className="profile-image"
-            loader={
-              <div className="profile-image-container-loading">
-                <Loading small />
-              </div>
-            }
-          />
-          <div className="profile-container-main">
-            <h2 className="profile-name">{data.name}</h2>
-          </div>
-          <div className="container-open">
-            {data.send === "encargue" && (
-              <>
-                <p className="profile-state-send state-encargue">
-                  SÓLO ENCARGUE PREVIO
-                </p>
-                <p className="info-encargue">
-                  Los lugares con 'sólo encargue previo' realizan envíos
-                  determinados días con previo encargue
-                </p>
-              </>
-            )}
-          </div>
-          {data.address && (
-            <div>
-              <a
-                href="https://www.google.com.ar/maps/preview"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="profile-ubication"
-              >
-                {data.address}
-              </a>
-            </div>
-          )}
-          {data.phone && (
-            <div>
-              <a href={"tel:+549" + data.phone} className="profile-call">
-                <img src={phone} className="profile-phone" alt="Llamar" />
-                Llamar al {data.phone}
-              </a>
-            </div>
-          )}
-          {data.phone && (
-            <div>
-              <a
-                href={
-                  "https://api.whatsapp.com/send?phone=[+549][ " +
-                  data.phone +
-                  "]"
-                }
-                target="_blank"
-                rel="noopener noreferrer"
-                className="profile-wpp"
-              >
-                {/* <img src={phone} className="profile-phone" alt="Llamar" /> */}
-                Abrir WhatsApp
-              </a>
-            </div>
-          )}
-          <div className="container-redes">
-            {data.instagram && (
-              <div>
-                <a
-                  href={"https://instagram.com/" + data.instagram}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="container-red"
-                >
-                  <Instagram className="profile-red" />
-                  <span>Instagram</span>
-                </a>
-              </div>
-            )}
-            {data.facebook && (
-              <div>
-                <a
-                  href={"https://www.facebook.com/" + data.facebook}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="container-red"
-                >
-                  <Facebook className="profile-red" />
-                  <span>Facebook</span>
-                </a>
-              </div>
-            )}
-            {data.whatsapp && (
-              <div>
-                <a
-                  href={"https://wa.me/" + data.whatsapp}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="container-red"
-                >
-                  <Whatsapp className="profile-red" />
-                  <span>Whatsapp</span>
-                </a>
-              </div>
-            )}
-            <div>
-              <a
-                href={
-                  "https://api.whatsapp.com/send?text=" +
-                  data.name.replace(/\s/g, "%20") +
-                  "%20https%3A%2F%pinamar-pide.web.app%2F%23%2F" +
-                  id +
-                  "%20"
-                }
-                target="_blank"
-                rel="noopener noreferrer"
-                className="container-red"
-              >
-                <Share className="profile-red share-icon" />
-                <span>Compartir</span>
-              </a>
-            </div>
-          </div>
-          {data.location && data.urlLocation && (
-            <div>
-              <a
-                href={data.urlLocation}
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ marginBottom: "15px" }}
-                className="container-workhours"
-              >
-                <Location className="workhours-icon" />
-                <p className="workhours-hours">{data.location}</p>
-              </a>
-            </div>
-          )}
 
-          {data.location && !data.urlLocation && (
-            <div
-              style={{ marginBottom: "15px" }}
-              className="container-workhours"
-            >
-              <Location className="workhours-icon" />
-              <p className="workhours-hours">{data.location}</p>
-            </div>
-          )}
-
-          {data.desc && <p className="profile-desc">{data.desc}</p>}
+      <div className="profile-container-place fade-in">
+        <div className="profile-container-main">
+          <h2 className="profile-name">Editar</h2>
         </div>
-      )}
+        <div className="container-open">
+          {data.send === "encargue" && (
+            <>
+              <p className="profile-state-send state-encargue">
+                SÓLO ENCARGUE PREVIO
+              </p>
+              <p className="info-encargue">
+                Los lugares con 'sólo encargue previo' realizan envíos
+                determinados días con previo encargue
+              </p>
+            </>
+          )}
+        </div>
+        <div>
+          <input
+            className="profile-call"
+            name="name"
+            type="text"
+            placeholder="Nombre"
+          />
+        </div>
+        <div>
+          <input
+            className="profile-call"
+            name="phone"
+            type="text"
+            placeholder="Teléfono"
+          />
+        </div>
+        <div>
+          <input
+            className="profile-call"
+            name="whatsapp"
+            type="text"
+            placeholder="Whatsapp"
+          />
+        </div>
+      </div>
     </div>
   );
 }
