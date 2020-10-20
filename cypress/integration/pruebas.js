7b1f54216d03a6acff3a2a91c134ef15828f6775
Octavio Amietta <octavioa1997@gmail.com>
2020-10-20 11:24:00 -0300

    add cypress

---
+++ cypress/integration/pruebas.js
@@ -0,0 +1,7 @@
+describe('Cuentas', function() {
+    var a = 1+1;
+    var b = 2;
+    it('Vamos a ver que se sume bien', function() {
+        expect(a===b).to.equal(true);
+    })
+})
---
+++ cypress/integration/search.js
@@ -0,0 +1,59 @@
+/// <reference types="cypress" />
+
+context("Actions", () => {
+  beforeEach(() => {
+    cy.visit("https://pinamar-pide.web.app/#/");
+  });
+
+  // https://on.cypress.io/interacting-with-element
+
+  it("Entra a comida, entra al primer comercio y vuelve a inicio", () => {
+    cy.get('[href="#/comida"]').click();
+    cy.get(":nth-child(1) > .container-list-item").click();
+    cy.get(".back").click();
+    expect(".back");
+  });
+  it("Entra a helados, busca Fre y abre wpp", () => {
+    cy.get('[href="#/helado"]').click();
+    cy.get(".search-input").type("Fre");
+    cy.get("rect").click();
+    expect("rect");
+  });
+
+  // You can click on 9 specific positions of an element:
+  //  -----------------------------------
+  // | topLeft        top       topRight |
+  // |                                   |
+  // |                                   |
+  // |                                   |
+  // | left          center        right |
+  // |                                   |
+  // |                                   |
+  // |                                   |
+  // | bottomLeft   bottom   bottomRight |
+  //  -----------------------------------
+
+  // clicking in the center of the element is the default
+  /* cy.get('#action-canvas').click()
+  
+      cy.get('#action-canvas').click('topLeft')
+      cy.get('#action-canvas').click('top')
+      cy.get('#action-canvas').click('topRight')
+      cy.get('#action-canvas').click('left')
+      cy.get('#action-canvas').click('right')
+      cy.get('#action-canvas').click('bottomLeft')
+      cy.get('#action-canvas').click('bottom')
+      cy.get('#action-canvas').click('bottomRight') */
+
+  // .click() accepts an x and y coordinate
+  // that controls where the click occurs :)
+
+  /* cy.get('#action-canvas')
+        .click(80, 75) // click 80px on x coord and 75px on y coord
+        .click(170, 75)
+        .click(80, 165)
+        .click(100, 185)
+        .click(125, 190)
+        .click(150, 185)
+        .click(170, 165) */
+});
